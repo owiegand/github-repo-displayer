180bdc835c7177b49296b1283821272bb6dfdc67
Oliver Wiegand <owiegand@gmail.com>
2017-02-14 17:49:20 -0500

    Finsihed extracting all the data from the API. I also set up a markdown for use inside of the readme file.

--- getRepos.php
+++ getRepos.php
@@ -1,34 +1,50 @@
 <?php
-
-$data = array("bio" => "This is my bio" );
-
+include('userAccountInfo.php');
 
 
-function curl($url, $data) {
-	include('userAccountInfo.php');
-	 $data_string = json_encode($data);
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,$url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET"); 
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);  
 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); 
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,1);
-    //curl_setopt($ch, CURLOPT_USERPWD, $GitHubUserName.":".$GithubPassword);
-	 curl_setopt($ch, CURLOPT_USERPWD, $GitHubUserName.":".$GithubPassword);
-	curl_setopt( $ch, CURLOPT_USERAGENT, $GitHubUserName);
-	var_dump( $GitHubUserName);
+//$data_string = json_encode($data);
+$url = 'https://api.github.com/users/'.$GitHubUserName.'/repos';
 
-    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);  
-	 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));  
-    $content = curl_exec($ch);
-    curl_close($ch);
-	var_dump($content);
-    return( $content);
-}
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET"); 
+//curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);  
 
-$result = json_decode(curl('https://api.github.com/users/owiegand/repos', $data),true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); 
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,1);
+curl_setopt($ch, CURLOPT_USERPWD, $GitHubUserName.":".$GithubPassword);
+curl_setopt( $ch, CURLOPT_USERAGENT, $GitHubUserName);
 
-var_dump($result);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);  
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));  
+$content = curl_exec($ch);
+curl_close($ch);
+
+$result = json_decode($content,true);
+
+$RepoName = $result[0]['name'];
+$RepoDes = $result[0]['description'];
+$RepoURL = $result[0]['html_url'];
+$RepoLangauge = $result[0]['language'];
+$RepoAPIURL = $result[0]['url'];
+$ReadMeURL = $RepoAPIURL.'/readme';
+
+$ch = curl_init();
+$timeout = 5;
+curl_setopt($ch, CURLOPT_URL, $ReadMeURL);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+curl_setopt( $ch, CURLOPT_USERAGENT, $GitHubUserName);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));  
+$data = curl_exec($ch);
+curl_close($ch);
+
+$DataArray = json_decode($data,true);
+$ReadMeText = trim(preg_split('/\[\*\*GITHUBPHPPARSER\*\*\]/', base64_decode($DataArray["content"]))[1]);
+$ReadMeText = preg_replace('/-->/', '', $ReadMeText);
+$RepoOptions = json_decode($ReadMeText,true);
+var_dump($RepoOptions);
+
 
 ?>

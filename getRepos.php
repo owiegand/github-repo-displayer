24957ea87becd5c190592a76a3eaef267c7441fc
Oliver Wiegand <OWWIEGAND@mail.wvu.edu>
2017-02-15 07:47:12 -0500

    Combined all data fields into a single output.

--- getRepos.php
+++ getRepos.php
@@ -1,10 +1,8 @@
 <?php
-include('userAccountInfo.php');
-
-
+include('userAccountInfo.php'); //Add Vars Containg User Account Info
 
 //$data_string = json_encode($data);
-$url = 'https://api.github.com/users/'.$GitHubUserName.'/repos';
+$url = 'https://api.github.com/users/'.$GitHubUserName.'/repos'; //API URL to get a users repos. This currently only returns public repos.
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$url);
@@ -41,10 +39,43 @@ $data = curl_exec($ch);
 curl_close($ch);
 
 $DataArray = json_decode($data,true);
+//Finds The Start of the Github Parser Info
+/*TODO: Make Sure When Stop at the Closing Tag
+Use This Code Instead
+$startsAt = strpos($out, "{FINDME}") + strlen("{FINDME}");
+$endsAt = strpos($out, "{/FINDME}", $startsAt);
+$result = substr($out, $startsAt, $endsAt - $startsAt);
+*/
 $ReadMeText = trim(preg_split('/\[\*\*GITHUBPHPPARSER\*\*\]/', base64_decode($DataArray["content"]))[1]);
 $ReadMeText = preg_replace('/-->/', '', $ReadMeText);
 $RepoOptions = json_decode($ReadMeText,true);
-var_dump($RepoOptions);
+
+
+$RepoInfo = array();
+//Options Coming From the Github API
+$RepoInfo['name'] = $RepoName;
+$RepoInfo['description'] = $RepoDes;
+$RepoInfo['html_url'] = $RepoURL;
+$RepoInfo['language'] = $RepoLangauge;
+
+//Custom Options Coming From The Readme File
+if($RepoOptions['AltShortDesc'] != NULL){
+	$RepoInfo['AltShortDesc'] = $RepoOptions['AltShortDesc'];
+}
+if($RepoOptions['LongDesc'] != NULL){
+	$RepoInfo['LongDesc'] = $RepoOptions['LongDesc'];
+}
+if($RepoOptions['ImgURL'] != NULL){
+	$RepoInfo['ImgURL'] = $RepoOptions['ImgURL'];
+}
+if($RepoOptions['ThumbailImgURL'] != NULL){
+	$RepoInfo['ThumbailImgURL'] = $RepoOptions['ThumbailImgURL'];
+}
+if($RepoOptions['Category'] != NULL){
+	$RepoInfo['Category'] = $RepoOptions['Category'];
+}
+
+print_r($RepoInfo);
 
 
 ?>

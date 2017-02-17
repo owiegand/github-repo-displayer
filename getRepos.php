aaaa8756a8eb05936d32951e4e71dd498032431c
Oliver Wiegand <OWWIEGAND@mail.wvu.edu>
2017-02-17 15:52:50 -0500

    Added code to allow the libaray to return data on all repos and not just the first one.

--- getRepos.php
+++ getRepos.php
@@ -19,65 +19,71 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 $content = curl_exec($ch);
 curl_close($ch);
 
-$result = json_decode($content,true);
+$result = json_decode($content,true); //Array Containing all public repos owned by the user.
 
-$RepoName = $result[0]['name'];
-$RepoDes = $result[0]['description'];
-$RepoURL = $result[0]['html_url'];
-$RepoLangauge = $result[0]['language'];
-$RepoAPIURL = $result[0]['url'];
-$ReadMeURL = $RepoAPIURL.'/readme';
+$AllRepoInfo = array();
 
-$ch = curl_init();
-$timeout = 5;
-curl_setopt($ch, CURLOPT_URL, $ReadMeURL);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-curl_setopt( $ch, CURLOPT_USERAGENT, $GitHubUserName);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));  
-$data = curl_exec($ch);
-curl_close($ch);
+foreach($result as $SingleRepo){
+	$RepoName = $SingleRepo['name'];
+	$RepoDes = $SingleRepo['description'];
+	$RepoURL = $SingleRepo['html_url'];
+	$RepoLangauge = $SingleRepo['language'];
+	$RepoAPIURL = $SingleRepo['url'];
+	$ReadMeURL = $RepoAPIURL.'/readme';
+	
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch, CURLOPT_URL, $ReadMeURL);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	curl_setopt( $ch, CURLOPT_USERAGENT, $GitHubUserName);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));  
+	$data = curl_exec($ch);
+	curl_close($ch);
+	
+	$DataArray = json_decode($data,true);
 
-$DataArray = json_decode($data,true);
-//Finds The Start of the Github Parser Info
-//TODO: Make Sure When Stop at the Closing Tag
-
-$ReadMeText = base64_decode($DataArray["content"]);
-
-//Extract The Data From Readme file between two tags.
-$startsAt = strpos($ReadMeText , "[**GITHUBPHPPARSER**]") + strlen("[**GITHUBPHPPARSER**]");
-$endsAt = strrpos($ReadMeText, "[**GITHUBPHPPARSER**]");
-$result = substr($ReadMeText, $startsAt, $endsAt-$startsAt);
-
-$RepoOptions = json_decode($result,true);
-
-
-$RepoInfo = array();
-
-//Options Coming From the Github API
-$RepoInfo['name'] = $RepoName;
-$RepoInfo['description'] = $RepoDes;
-$RepoInfo['html_url'] = $RepoURL;
-$RepoInfo['language'] = $RepoLangauge;
-
-//Custom Options Coming From The Readme File
-if($RepoOptions['AltShortDesc'] != NULL){
-	$RepoInfo['AltShortDesc'] = $RepoOptions['AltShortDesc'];
+	$ReadMeText = base64_decode($DataArray["content"]);
+	
+	//Extract The Data From Readme file between two tags.
+	$startsAt = strpos($ReadMeText , "[**GITHUBPHPPARSER**]") + strlen("[**GITHUBPHPPARSER**]");
+	$endsAt = strrpos($ReadMeText, "[**GITHUBPHPPARSER**]");
+	$result = substr($ReadMeText, $startsAt, $endsAt-$startsAt);
+	
+	$RepoOptions = json_decode($result,true);
+	
+	
+	$RepoInfo = array();
+	
+	//Options Coming From the Github API
+	$RepoInfo['name'] = $RepoName;
+	$RepoInfo['description'] = $RepoDes;
+	$RepoInfo['html_url'] = $RepoURL;
+	$RepoInfo['language'] = $RepoLangauge;
+	
+	//Custom Options Coming From The Readme File
+	if($RepoOptions['AltShortDesc'] != NULL){
+		$RepoInfo['AltShortDesc'] = $RepoOptions['AltShortDesc'];
+	}
+	if($RepoOptions['LongDesc'] != NULL){
+		$RepoInfo['LongDesc'] = $RepoOptions['LongDesc'];
+	}
+	if($RepoOptions['ImgURL'] != NULL){
+		$RepoInfo['ImgURL'] = $RepoOptions['ImgURL'];
+	}
+	if($RepoOptions['ThumbailImgURL'] != NULL){
+		$RepoInfo['ThumbailImgURL'] = $RepoOptions['ThumbailImgURL'];
+	}
+	if($RepoOptions['Category'] != NULL){
+		$RepoInfo['Category'] = $RepoOptions['Category'];
+	}
+	$AllRepoInfo = $RepoInfo;
 }
-if($RepoOptions['LongDesc'] != NULL){
-	$RepoInfo['LongDesc'] = $RepoOptions['LongDesc'];
-}
-if($RepoOptions['ImgURL'] != NULL){
-	$RepoInfo['ImgURL'] = $RepoOptions['ImgURL'];
-}
-if($RepoOptions['ThumbailImgURL'] != NULL){
-	$RepoInfo['ThumbailImgURL'] = $RepoOptions['ThumbailImgURL'];
-}
-if($RepoOptions['Category'] != NULL){
-	$RepoInfo['Category'] = $RepoOptions['Category'];
-}
 
-print_r($RepoInfo);
+
+
+
+print_r($AllRepoInfo);
 
 
 ?>
